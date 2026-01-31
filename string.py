38a9e080bfb362ae8e90cd35875133d04a18fba2
zhhzhzhz <am_omirbek@kbtu.kz>
2026-01-31 12:34:07 +0500

    Add files via upload

--- string.py
+++ string.py
@@ -1,5 +1,21 @@
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
-a = "Hello"
-print(a)
+
+
+#slicing
+b = "Hello, World!"
+print(b[2:5])
+
+
+#uppercase
+a = "Hello, World!"
+print(a.upper())
+
+#lowercase
+a = "Hello, World!"
+print(a.lower())
+
+#убрать пробелы
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
