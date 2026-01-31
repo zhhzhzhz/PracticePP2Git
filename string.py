e210c821687319dcb6cf3124e73510eb11ed96e6
zhhzhzhz <am_omirbek@kbtu.kz>
2026-01-31 12:32:21 +0500

    strings in python

---
+++ string.py
@@ -0,0 +1,21 @@
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#slicing
+b = "Hello, World!"
+print(b[2:5])
+
+
+#uppercase
+a = "Hello, World!"
+print(a.upper())
+
+#lowercase
+a = "Hello, World!"
+print(a.lower())
+
+#убрать пробелы
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
